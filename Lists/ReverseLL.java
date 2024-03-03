fc7369ce8257b0f352db046b754bae50952ebe4f
Sahil Gedam <gedamsahil1997@gmail.com>
2024-03-03 09:27:28 +0530

    LinkedList

---
+++ Lists/ReverseLL.java
@@ -0,0 +1,38 @@
+// import java.util.*;
+
+class ListNode {
+
+  int val;
+  ListNode next;
+
+  ListNode(int x) {
+    val = x;
+  }
+}
+
+public class ReverseLL {
+
+  public ListNode reverseList(ListNode head) {
+    if (head == null || head.next == null) return head;
+
+    //use two nodes to keep track of two nodes
+    ListNode prev = head;
+    ListNode curr = head.next;
+
+    while (curr != null) {
+      //keep track of next node of curr, since we are going to change its next to prev
+      ListNode tmp = curr.next;
+
+      //reverse
+      curr.next = prev;
+
+      //shift right by one position
+      prev = curr;
+      curr = tmp;
+    }
+
+    //set tail
+    head.next = null;
+    return prev;
+  }
+}
