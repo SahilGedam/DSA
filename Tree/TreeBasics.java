05acdc0d10492a70259639602ad3924286f4ce97
Sahil Gedam <gedamsahil1997@gmail.com>
2024-03-08 23:21:47 +0530

    Tree

--- Tree/TreeBasics.java
+++ Tree/TreeBasics.java
@@ -33,10 +33,31 @@ public class TreeBasics {
     }
   }
 
+  public static void preorder(Node root) { // root first
+    if (root == null) {
+      System.out.print(-1 + " ");
+      return;
+    }
+    System.out.print(root.data + " ");
+    preorder(root.left);
+    preorder(root.right);
+  }
+
+  public static void inorder(Node root) { // print left nodes first
+    if (root == null) {
+      return;
+    }
+    inorder(root.left);
+    System.out.print(root.data + " ");
+    inorder(root.right);
+  }
+
   public static void main(String[] args) {
     int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
     BinaryTree tree = new BinaryTree();
     Node root = tree.buildTree(nodes);
-    System.out.println(root.data);
+    // System.out.println(root.data);
+    //  preorder(root); // O (n) traversal
+    inorder(root);
   }
 }
