2725b4b8586d819972cb59d662540ef9dbaa6d8b
Sahil Gedam <gedamsahil1997@gmail.com>
2024-03-09 00:01:05 +0530

    Tree

--- Tree/TreeBasics.java
+++ Tree/TreeBasics.java
@@ -1,5 +1,8 @@
 package Tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 // build tree from preorder
 public class TreeBasics {
 
@@ -61,6 +64,35 @@ public class TreeBasics {
     System.out.print(root.data + " ");
   }
 
+  public static void levelorder(Node root) { // traverse by levels
+    if (root == null) {
+      return;
+    }
+    Queue<Node> q = new LinkedList<>();
+    q.add(root);
+    q.add(null);
+
+    while (!q.isEmpty()) {
+      Node currNode = q.remove();
+      if (currNode == null) {
+        System.out.println();
+        if (q.isEmpty()) {
+          break;
+        } else {
+          q.add(null);
+        }
+      } else {
+        System.out.print(currNode.data + " ");
+        if (currNode.left != null) {
+          q.add(currNode.left);
+        }
+        if (currNode.right != null) {
+          q.add(currNode.right);
+        }
+      }
+    }
+  }
+
   public static void main(String[] args) {
     int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
     BinaryTree tree = new BinaryTree();
@@ -68,6 +100,7 @@ public class TreeBasics {
     // System.out.println(root.data);
     //  preorder(root); // O (n) traversal
     // inorder(root);
-    postorder(root);
+    // postorder(root);
+    levelorder(root);
   }
 }
