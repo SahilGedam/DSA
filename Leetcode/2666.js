7c1bf6d37ecdccbe421a6a6bf0c90f300d94112f
Sahil Gedam <gedamsahil1997@gmail.com>
2024-04-21 18:14:54 +0530

    JS

--- Leetcode/2666.js
+++ Leetcode/2666.js
@@ -15,10 +15,10 @@ var once = function (fn) {
     }
 };
 
-/**
- * let fn = (a,b,c) => (a + b + c)
- * let onceFn = once(fn)
- *
- * onceFn(1,2,3); // 6
- * onceFn(2,3,6); // returns undefined without calling fn
- */
+
+  let fn = (a,b,c) => (a + b + c)
+ let onceFn = once(fn)
+ console.log(onceFn(1,2,3));
+onceFn(1,2,3); // 6
+onceFn(2,3,6); // returns undefined without calling fn
+ 
