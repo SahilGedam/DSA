4a4a8d84f0b039618954464610ec11d4adc2e32c
Sahil Gedam <gedamsahil1997@gmail.com>
2024-04-25 10:30:12 +0530

    cache & promise

--- Leetcode/2623.memoise.js
+++ Leetcode/2623.memoise.js
@@ -11,11 +11,11 @@ function memoize(fn) {
   };
 }
 
-let callCount = 0;
-const memoizedFn = memoize(function (a, b) {
-  callCount += 1;
-  return a + b;
-});
-memoizedFn(2, 3); // 5
-memoizedFn(2, 3); // 5
+// let callCount = 0;
+// const memoizedFn = memoize(function (a, b) {
+//   callCount += 1;
+//   return a + b;
+// });
+// memoizedFn(2, 3); // 5
+// memoizedFn(2, 3); // 5
 // console.log(callCount) // 1
